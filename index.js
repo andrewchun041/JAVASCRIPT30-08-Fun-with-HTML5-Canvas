0da35fc8d7215b8bf23fa47362351a4a65d1c1c2
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-11 19:58:29 -0700

    make drawing color change from hsl 0 to 360 as it's being drawn; make stroke size increase and decrease as it's being drawn

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ ctx.lineWidth = 100;
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
     if (!isDrawing) return; // stop the fn from running when not moused down
-    console.log(e);
+
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+
     ctx.beginPath();
     // start from
     ctx.moveTo(lastX, lastY);
@@ -21,7 +25,25 @@ function draw(e) {
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
 
+    // set new canvas position
     [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    // increment hue and reset to 0 at 360
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+
+    // switch increment/decrement direction
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+    // increment/decrement lineWidth
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
 }
 
 canvas.addEventListener('mousedown', (e) => {
