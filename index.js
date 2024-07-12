0cdde7dc5ec5be753b8e81189a82dbe51e091ae3
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-11 19:38:55 -0700

    add ctx methods in draw function to invoke drawing/line paths to occur

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
@@ -13,9 +14,21 @@ let lastY = 0;
 function draw(e) {
     if (!isDrawing) return; // stop the fn from running when not moused down
     console.log(e);
+    ctx.beginPath();
+    // start from
+    ctx.moveTo(lastX, lastY);
+    // go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
